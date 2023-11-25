bfaf479259867ffe6e921f84ade93ad044cfbe7a
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-26 00:10:27 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Good Luck 💪🏾
+function A(list){
+    let LargestArray = [];
+
+    for(let i=0; i<list.length; i++)
+    {
+        LargestArray.push(Math.max(...list[i]))
+    
+        
+    }
+
+    return (LargestArray)
+}
+
+console.log(A([[7, 3, 8, 15], [54, 12, 43, 99], [21, 11, 8]]))    //➞ [15, 99, 21]
+console.log(A([[22, 59, 12], [45, 66, 90], [28, 5, 11]])) ; // ➞ [59, 90, 28]
